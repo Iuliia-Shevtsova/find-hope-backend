bc2c3effb558380f7944699a6727e88013d2fc84
Iuliia Shevtsova <uliyaal2018@gmail.com>
2021-01-21 22:09:36 -0500

    no changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,47 +6,6 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# categories = Category.create([
-#     {
-#       name: "Category 1",
-#     },
-#     {
-#       name: "Category 2",
-#     },
-#     {
-#       name: "Category 3",
-#     },
-# ])
-
-# organizations = Organization.create([
-#     {
-#       name: "Organization 1",
-#       address: 'NY', 
-#       zipCode:'10001', 
-#       website: 'website 1', 
-#       facebook: 'facebook 1', 
-#       twitter: 'twitter 1',
-#       categories: [
-#         Category.find(1),
-#         Category.find(3)
-#       ]
-#     },
-#     {
-#       name: "Organization 2",
-#       address: 'NY', 
-#       zipCode:'10002', 
-#       website: 'website 2', 
-#       facebook: 'facebook 2', 
-#       twitter: 'twitter 2',
-#       categories: [
-#         Category.find(2),
-#         Category.find(1)
-#       ]
-#     }
-# ])
-
-
-
 categories = Category.create([
   {
     name: "Legal Services",
